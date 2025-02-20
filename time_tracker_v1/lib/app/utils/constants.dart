d1bddfd2846718e2cfd597f592009be2b1db4e99
omarragab2020 <omarragab712000@gmail.com>
2025-02-20 07:00:32 +0200

    h
    jh

--- time_tracker_v1/lib/app/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-class Constants {
-  static const String appName = 'Time Tracker';
-  static const String logoTag = 'near.huscarl.loginsample.logo';
-  static const String titleTag = 'near.huscarl.loginsample.title';
-}
